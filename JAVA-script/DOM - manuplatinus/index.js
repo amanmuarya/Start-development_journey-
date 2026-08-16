7c3121270d0c28fb29880c2ca3485e1ebdfad665
amanmuarya <kushwahaaman57957@gmail.com>
2026-08-16 20:59:38 +0530

    update all file

---
+++ JAVA-script/DOM - manuplatinus/index.js
@@ -0,0 +1,15 @@
+const bodyBackground = document.body;
+bodyBackground.backgroundColor ="red "
+
+// 1. approch 
+
+// const box4 = document.getElementById('box-4');
+// console.log(box4);
+
+// 2. approch 
+const h1=document.gitElementByclassName('box');
+console.log(h1);
+ 
+// 3. approch 
+const h1= docoument.gitElementByclassName('box');
+console.log(boxes );
